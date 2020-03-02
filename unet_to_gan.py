4e11845926e49c6864da0536d9bd27263757a249
n-mevada <n.mevada@partner.samsung.com>
2020-03-02 13:59:04 +0530

    Initialization

--- unet_to_gan.py
+++ unet_to_gan.py
@@ -38,98 +38,114 @@ def generator_model():
     model_in = layers.Input((720, 1280, 3))
     
     #down
-    c1 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(model_in)
-    c1 = layers.BatchNormalization()(c1)
-    c1 = layers.Activation('relu')(c1)
-    c1 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c1)
-    c1 = layers.BatchNormalization()(c1)
-    c1 = layers.Activation('relu')(c1)
+    with tf.device('/gpu:1'):
+        c1 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(model_in)
+        c1 = layers.BatchNormalization()(c1)
+        c1 = layers.Activation('relu')(c1)
+        c1 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c1)
+        c1 = layers.BatchNormalization()(c1)
+        c1 = layers.Activation('relu')(c1)
 
-    p1 = layers.MaxPooling2D((2,2))(c1)
-    p1 = layers.Dropout(0.1)(p1)
+        p1 = layers.MaxPooling2D((2,2))(c1)
+        p1 = layers.Dropout(0.1)(p1)
 
-    c2 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p1)
-    c2 = layers.BatchNormalization()(c2)
-    c2 = layers.Activation('relu')(c2)
-    c2 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c2)
-    c2 = layers.BatchNormalization()(c2)
-    c2 = layers.Activation('relu')(c2)
+        c2 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p1)
+        c2 = layers.BatchNormalization()(c2)
+        c2 = layers.Activation('relu')(c2)
+        c2 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c2)
+        c2 = layers.BatchNormalization()(c2)
+        c2 = layers.Activation('relu')(c2)
 
-    p2 = layers.MaxPooling2D((2,2))(c2)
-    p2 = layers.Dropout(0.1)(p2)
+        p2 = layers.MaxPooling2D((2,2))(c2)
+        p2 = layers.Dropout(0.1)(p2)
+        
 
-    c3 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p2)
-    c3 = layers.BatchNormalization()(c3)
-    c3 = layers.Activation('relu')(c3)
-    c3 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c3)
-    c3 = layers.BatchNormalization()(c3)
-    c3 = layers.Activation('relu')(c3)
-
-    p3 = layers.MaxPooling2D((2,2))(c3)
-    p3 = layers.Dropout(0.1)(p3)
-
-    c4 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p3)
-    c4 = layers.BatchNormalization()(c4)
-    c4 = layers.Activation('relu')(c4)
-    c4 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c4)
-    c4 = layers.BatchNormalization()(c4)
-    c4 = layers.Activation('relu')(c4)
-
-    p4 = layers.MaxPooling2D((2,2))(c4)
-    p4 = layers.Dropout(0.1)(p4)
-
-    c5 = layers.Conv2D(256, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p4)
-    c5 = layers.BatchNormalization()(c5)
-    c5 = layers.Activation('relu')(c5)
-    c5 = layers.Conv2D(256, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c5)
-    c5 = layers.BatchNormalization()(c5)
-    c5 = layers.Activation('relu')(c5)
-
-
-    noise = tf.random.normal(tf.shape(c5))
-    c5 = layers.concatenate([c5, noise])
     
+        c3 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p2)
+        c3 = layers.BatchNormalization()(c3)
+        c3 = layers.Activation('relu')(c3)
+        c3 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c3)
+        c3 = layers.BatchNormalization()(c3)
+        c3 = layers.Activation('relu')(c3)
+
+        p3 = layers.MaxPooling2D((2,2))(c3)
+        p3 = layers.Dropout(0.1)(p3)
+
+    
+        c4 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p3)
+        c4 = layers.BatchNormalization()(c4)
+        c4 = layers.Activation('relu')(c4)
+        c4 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c4)
+        c4 = layers.BatchNormalization()(c4)
+        c4 = layers.Activation('relu')(c4)
+        
+    with tf.device('/gpu:2'):
+    
+        p4 = layers.MaxPooling2D((2,2))(c4)
+        p4 = layers.Dropout(0.1)(p4)
+
+        c5 = layers.Conv2D(256, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(p4)
+        c5 = layers.BatchNormalization()(c5)
+        c5 = layers.Activation('relu')(c5)
+        
+    with tf.device('/gpu:3'):
+            
+        c5 = layers.Conv2D(256, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c5)
+        c5 = layers.BatchNormalization()(c5)
+        c5 = layers.Activation('relu')(c5)
+
+
+        noise = tf.random.normal(tf.shape(c5))
+        c5 = layers.concatenate([c5, noise])
+        
     
     #up
-    u6 = layers.Conv2DTranspose(128, (2, 2), strides = (2,2), padding = 'same')(c5)
-    u6 = layers.concatenate([u6, c4])
-    u6 = layers.Dropout(0.1)(u6)
-    c6 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u6)
-    c6 = layers.BatchNormalization()(c6)
-    c6 = layers.Activation('relu')(c6)
-    c6 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c6)
-    c6 = layers.BatchNormalization()(c6)
-    c6 = layers.Activation('relu')(c6) 
+    with tf.device('/gpu:4'):
+        u6 = layers.Conv2DTranspose(128, (2, 2), strides = (2,2), padding = 'same')(c5)
+        u6 = layers.concatenate([u6, c4])
+        u6 = layers.Dropout(0.1)(u6)
+    
+    with tf.device('/gpu:5'):
+        c6 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u6)
+        c6 = layers.BatchNormalization()(c6)
+        c6 = layers.Activation('relu')(c6)
+    
+    with tf.device('/gpu:6'):
+        c6 = layers.Conv2D(128, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c6)
+        c6 = layers.BatchNormalization()(c6)
+        c6 = layers.Activation('relu')(c6) 
 
-    u7 = layers.Conv2DTranspose(64, (2, 2), strides = (2,2), padding = 'same')(c6)
-    u7 = layers.concatenate([u7, c3])
-    u7 = layers.Dropout(0.1)(u7)
-    c7 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u7)
-    c7 = layers.BatchNormalization()(c7)
-    c7 = layers.Activation('relu')(c7)
-    c7 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c7)
-    c7 = layers.BatchNormalization()(c7)
-    c7 = layers.Activation('relu')(c7) 
+        u7 = layers.Conv2DTranspose(64, (2, 2), strides = (2,2), padding = 'same')(c6)
+        u7 = layers.concatenate([u7, c3])
+        u7 = layers.Dropout(0.1)(u7)
+        c7 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u7)
+        c7 = layers.BatchNormalization()(c7)
+        c7 = layers.Activation('relu')(c7)
+        c7 = layers.Conv2D(64, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c7)
+        c7 = layers.BatchNormalization()(c7)
+        c7 = layers.Activation('relu')(c7) 
 
-    u8 = layers.Conv2DTranspose(32, (2, 2), strides = (2,2), padding = 'same')(c7)
-    u8 = layers.concatenate([u8, c2])
-    u8 = layers.Dropout(0.1)(u8)
-    c8 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u8)
-    c8 = layers.BatchNormalization()(c8)
-    c8 = layers.Activation('relu')(c8)
-    c8 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c8)
-    c8 = layers.BatchNormalization()(c8)
-    c8 = layers.Activation('relu')(c8)
+    
+        u8 = layers.Conv2DTranspose(32, (2, 2), strides = (2,2), padding = 'same')(c7)
+        u8 = layers.concatenate([u8, c2])
+        u8 = layers.Dropout(0.1)(u8)
+        c8 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u8)
+        c8 = layers.BatchNormalization()(c8)
+        c8 = layers.Activation('relu')(c8)
+        c8 = layers.Conv2D(32, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c8)
+        c8 = layers.BatchNormalization()(c8)
+        c8 = layers.Activation('relu')(c8)
 
-    u9 = layers.Conv2DTranspose(16, (2, 2), strides = (2,2), padding = 'same')(c8)
-    u9 = layers.concatenate([u9, c1])
-    u9 = layers.Dropout(0.1)(u9)
-    c9 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u9)
-    c9 = layers.BatchNormalization()(c9)
-    c9 = layers.Activation('relu')(c9)
-    c9 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c9)
-    c9 = layers.BatchNormalization()(c9)
-    c9 = layers.Activation('relu')(c9)
+    
+        u9 = layers.Conv2DTranspose(16, (2, 2), strides = (2,2), padding = 'same')(c8)
+        u9 = layers.concatenate([u9, c1])
+        u9 = layers.Dropout(0.1)(u9)
+        c9 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(u9)
+        c9 = layers.BatchNormalization()(c9)
+        c9 = layers.Activation('relu')(c9)
+        c9 = layers.Conv2D(16, (3, 3), kernel_initializer = 'he_normal', padding = 'same')(c9)
+        c9 = layers.BatchNormalization()(c9)
+        c9 = layers.Activation('relu')(c9)
 
     model_out = layers.Conv2D(3, (1,1), activation = 'sigmoid')(c9)
     model = tf.keras.Model(inputs = [model_in], outputs = [model_out])
@@ -141,33 +157,34 @@ def discriminator_model():
     input1 = layers.Input((720, 1280, 3))
     input2 = layers.Input((720, 1280, 3))
 
-    c11 = layers.Conv2D(64, (3, 3), strides = (2, 2), padding = 'same', input_shape = [720, 1280, 3])(input1)
-    c11 = layers.LeakyReLU()(c11)
-    c11 = layers.Dropout(0.15)(c11)
-    
-    c12 = layers.Conv2D(64, (3, 3), strides = (2, 2), padding = 'same', input_shape = [720, 1280, 3])(input2)
-    c12 = layers.LeakyReLU()(c12)
-    c12 = layers.Dropout(0.15)(c12)
-    
-    c21 = layers.Conv2D(16, (3, 3), strides = (2, 2), padding = 'same')(c11)
-    c21 = layers.LeakyReLU()(c21)
-    c21 = layers.Dropout(0.15)(c21)
-    
-    c22 = layers.Conv2D(16, (3, 3), strides = (2, 2), padding = 'same')(c12)
-    c22 = layers.LeakyReLU()(c22)
-    c22 = layers.Dropout(0.15)(c22)
-    
-    c3 = layers.concatenate([c21, c22])
-    c3 = layers.Conv2D(8, (3, 3), strides = (2, 2), padding = 'same')(c3)
-    c3 = layers.LeakyReLU()(c3)
-    c3 = layers.Dropout(0.15)(c3)
-    
-    c4 = layers.Conv2D(4, (3, 3), strides = (2, 2), padding = 'same')(c3)
-    c4 = layers.LeakyReLU()(c4)
-    c4 = layers.Dropout(0.15)(c4)
-    
-    fla = layers.Flatten()(c4)
-    out = layers.Dense(1)(fla)
+    with tf.device('/gpu:7'):
+        c11 = layers.Conv2D(64, (3, 3), strides = (2, 2), padding = 'same', input_shape = [720, 1280, 3])(input1)
+        c11 = layers.LeakyReLU()(c11)
+        c11 = layers.Dropout(0.15)(c11)
+        
+        c12 = layers.Conv2D(64, (3, 3), strides = (2, 2), padding = 'same', input_shape = [720, 1280, 3])(input2)
+        c12 = layers.LeakyReLU()(c12)
+        c12 = layers.Dropout(0.15)(c12)
+        
+        c21 = layers.Conv2D(16, (3, 3), strides = (2, 2), padding = 'same')(c11)
+        c21 = layers.LeakyReLU()(c21)
+        c21 = layers.Dropout(0.15)(c21)
+        
+        c22 = layers.Conv2D(16, (3, 3), strides = (2, 2), padding = 'same')(c12)
+        c22 = layers.LeakyReLU()(c22)
+        c22 = layers.Dropout(0.15)(c22)
+        
+        c3 = layers.concatenate([c21, c22])
+        c3 = layers.Conv2D(8, (3, 3), strides = (2, 2), padding = 'same')(c3)
+        c3 = layers.LeakyReLU()(c3)
+        c3 = layers.Dropout(0.15)(c3)
+        
+        c4 = layers.Conv2D(4, (3, 3), strides = (2, 2), padding = 'same')(c3)
+        c4 = layers.LeakyReLU()(c4)
+        c4 = layers.Dropout(0.15)(c4)
+        
+        fla = layers.Flatten()(c4)
+        out = layers.Dense(1)(fla)
     
     model = tf.keras.Model(inputs = [input1, input2], outputs = [out])
     return model
