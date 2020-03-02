6aa4b422d4c720b5160de4c556cf5198f6ad390a
n-mevada <n.mevada@partner.samsung.com>
2020-03-02 14:55:12 +0530

    Initialization

--- unet_to_gan.py
+++ unet_to_gan.py
@@ -17,13 +17,14 @@ from tensorflow.keras import layers
 
 hvd.init()
 
-gpus = tf.config.experimental.list_physical_devices()
+gpupluscpu = tf.config.experimental.list_physical_devices()
+gpus = tf.config.experimental.list_physical_devices('GPU')
 print("Gpu's for horovod: ")
 print(gpus)
 
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-tf.config.experimental.set_visible_devices(gpus)
+tf.config.experimental.set_visible_devices(gpuspluscpu)
 tf.debugging.set_log_device_placement(True)
 
 
