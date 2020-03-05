fixed normalization added parser, epochs = 3

unet_to_gan.py
@@ -31,7 +31,13 @@ tf.debugging.set_log_device_placement(True)
 #constants
 
 BATCH_SIZE = 1
-EPOCHS = 150
+EPOCHS = 3
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument("-e", "--epochs", type = int, dest = 'epochs', help = "Number of epochs", default = EPOCHS)
+    return parser
+
 
 
 def generator_model():
@@ -213,8 +219,8 @@ def train():
             
             x_batch = [np.asarray(Image.open(f)) for f in x_batch.numpy()]
             y_batch = [np.asarray(Image.open(f)) for f in y_batch.numpy()]
-            x_batch = [((x - 127.5) / 127.5) for x in x_batch]
-            y_batch = [((x - 127.5) / 127.5) for x in y_batch]  
+            x_batch = [(x / 255) for x in x_batch]
+            y_batch = [(x / 255) for x in y_batch]  
             x_batch = np.asarray(x_batch)
             y_batch = np.asarray(y_batch)
             x_batch = tf.convert_to_tensor(x_batch, dtype=tf.float32)
@@ -256,7 +262,7 @@ def test():
     x_name = [f for f in glob.glob('./val_blur/' + "**/*.png", recursive = True)]
     x_name = x_name[0:20]
     x_test = [np.asarray(Image.open(f)) for f in x_name]
-    x_test = [((x - 127.5) / 127.5) for x in x_test]
+    x_test = [(x / 255) for x in x_test]
     it = 1
     for x in x_test:
         inp = tf.convert_to_tensor(x, dtype=tf.float32)
@@ -269,6 +275,10 @@ def test():
         x = Image.fromarray((x*255).astype(np.uint8))
         x.save('./tmp/'+str(it)+'_inp.png')
         it += 1
+
+parser = build_parser()
+options = parser.parse_args()
+EPOCHS = options.epochs
 
 train_dataset = load_data(BATCH_SIZE)
 log_array = []