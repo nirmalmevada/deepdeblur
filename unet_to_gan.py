c85f0591cc074fa7b2b80d8e747edc9f7b1d2ff2
n-mevada <n.mevada@partner.samsung.com>
2020-03-06 15:25:10 +0530

    leakyrelu-tanh heatmapfunc

--- unet_to_gan.py
+++ unet_to_gan.py
@@ -25,7 +25,9 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 if gpus:
     tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
-tf.debugging.set_log_device_placement(True)
+    
+#toprintlogs
+#tf.debugging.set_log_device_placement(True)
 
 
 #constants
