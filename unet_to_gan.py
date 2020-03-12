fixed gen_loss and heatmap scaling for input

unet_to_gan.py
@@ -419,6 +419,7 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
 
 load_checkpoint()
 train()
-test()
+if hvd.rank == 0:
+    test()
 suffix = int(random()*10000)
 np.save('./tmp/log_array_'+str(suffix)+'.npy', log_array)