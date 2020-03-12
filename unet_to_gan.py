fixed gen_loss and heatmap scaling for input

unet_to_gan.py
@@ -249,8 +249,8 @@ l2_loss = tf.keras.losses.MeanSquaredError()
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True)
 
 def discriminator_loss(real_output , generated_output):
-    real_loss = cross_entropy( (np.random.random((real_output.shape))*0.3 + 0.7) * tf.ones_like(real_output), real_output)
-    fake_loss = cross_entropy( (np.random.random((generated_output.shape))*0.3) * tf.zeros_like(generated_output), generated_output)
+    real_loss = cross_entropy( (np.random.random((real_output.shape))*0.1 + 0.9) * tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy( (np.random.random((generated_output.shape))*0.1) * tf.zeros_like(generated_output), generated_output)
     return real_loss, fake_loss
 
 def generator_loss(fake, sharp, dis_f_loss):
@@ -268,6 +268,7 @@ def heatmapwithoutabs(image1, image2):
     finalimage[:,:,0] = image1[:,:,0] - image2[:,:,0]
     finalimage[:,:,1] = image1[:,:,1] - image2[:,:,1]
     finalimage[:,:,2] = image1[:,:,2] - image2[:,:,2]
+    finalimage = finalimage / 2
     finalimage = tf.convert_to_tensor(finalimage, dtype=tf.float32)
     finalimage = tf.expand_dims(finalimage, 0)
     return finalimage
@@ -291,16 +292,18 @@ def train():
                 
                 gen_images = generator(x_batch, training = True)
                 
-                real_output = discriminator([x_batch, heatmapwithoutabs(x_batch, y_batch)], training = True)
+                hmapxy = heatmapwithoutabs(x_batch, y_batch)
+                real_output = discriminator([x_batch, hmapxy], training = True)
                 fake_output = discriminator([x_batch, gen_images], training = True)
                 
                 dis_r_loss, dis_f_loss = discriminator_loss(real_output, fake_output)
                 dis_loss = dis_r_loss + dis_f_loss
-                L1, L2, gen_loss = generator_loss(gen_images, y_batch, dis_f_loss)
+                L1, L2, gen_loss = generator_loss(gen_images, hmapxy, dis_f_loss)
                 
                 inpimg = x_batch.numpy()
                 expimg = y_batch.numpy()
                 genimg = gen_images.numpy()
+                genimg = inpimg - 2 * genimg
                 
                 inpimg = inpimg[0,:,:,:]
                 expimg = expimg[0,:,:,:]
@@ -373,7 +376,7 @@ def test():
         out = generator(inp, training = False)
         out = np.asarray(out)
         out = out[0,:,:,:]
-        out = x - out
+        out = x - 2 * out
         heatmapexp = heatmap(x, y)
         heatmapres = heatmap(x, out)    
         y = Image.fromarray(((y/2 + 0.5)*255).astype(np.uint8))